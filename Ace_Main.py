4ef88d9f9808020c8ddaa2f19fbd5fc44de94698
Kaiser Lee <kaiserkaiserlee@gmail.com>
2025-11-24 18:47:11 -0800

    added initial variables for main

--- Ace_Main.py
+++ Ace_Main.py
@@ -6,3 +6,17 @@ Created on Mon Nov 24 18:12:51 2025
 @author: kaiserlee
 """
 
+# Main file for the Acetaminophen Simulation Project
+
+# Import all other files and modules
+
+
+# Initialize constant variables
+time = 5                    #   amount of time in simulation
+time_interval = 0.1         #   amount of time passed per interval (total time steps = time/time_interval)
+
+dose_interval = 1           #   amount of time between dose
+dose_count = 3              #   amount of dose taken
+dose_amount = 0.150         #   amount taken per dose in [unit]
+
+# 

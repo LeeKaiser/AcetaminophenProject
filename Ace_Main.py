eedb01ae72bc99dded1e356f79c2f6738b54be69
Kaiser Lee <kaiserkaiserlee@gmail.com>
2025-11-24 18:15:58 -0800

    added starting files to complete first benchmark

---
+++ Ace_Main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 24 18:12:51 2025
+
+@author: kaiserlee
+"""
+
---
+++ Digestive_System.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 24 18:14:42 2025
+
+@author: kaiserlee
+"""
+
+#represents digestive system

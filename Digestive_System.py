bfc8da3f78622bf7de684310927fd45131ac4f75
Vivian <npellock@uw.edu>
2025-11-28 17:19:49 -0800

    finished intestine, revamped stomach, cleaned up some

--- Digestive_System.py
+++ Digestive_System.py
@@ -9,55 +9,89 @@ Created on Mon Nov 24 18:14:42 2025
 #represents digestive system
 import ace_global_vars as ACE
 import numpy as np
-#import Ace_Main as main
 
 isFed = False       #whether the individual has fasted or has eaten
-aceInStomach = 0    #Acetaminophen in the stomach
-aceInIntestine = 0  #Acetaminophen in the intestine
-lastDose = 0        #time since last dose, in minutes
+stomachDoses = []   #Acetaminophen in the stomach
+intestineDoses = [] #Acetaminophen in the intestine
 dosesTaken = 0      #doses taken so far
-#simTime = 0         #simulation time, in minutes
+simTime = 0         #simulation time, in minutes
+Ka = 2 / 60.0       #absorption constant of ace
 
 """
 takes the amount of time, in minutes, of the step
 returns the amount, in milligrams, of the ace being output to the blood during this step
 """
 def step(time=30):
-    #global simTime
     
     stomachStep(time)
-    intestineStep(time)
-    #simTime += time
-    
-    print("step in digestive system done")
+    absorbed = intestineStep(time)
+    return absorbed
     
 
+def giveDose():
+    global aceInStomach, dosesTaken
+    
+    mg = ACE.dose_amount * 1000.0
+    aceInStomach += mg
+    dosesTaken += 1
+    
+def checkAutoDosing():
+    global simTime, dosesTaken
+    
+    if dosesTaken < ACE.dose_count:
+        nextDoseTime = dosesTaken * (ACE.dose_interval * 60)
+        if simTime >= nextDoseTime:
+            giveDose()
+
+
 def stomachStep(time):
-    global lastDose
-    global aceInIntestine
-    global aceInStomach
-    #global simTime
-    global dosesTaken
+    global simTime, stomachDoses, intestineDoses, isFed
     
-    if dosesTaken < ACE.dose_count and lastDose == ACE.dose_interval:
-        aceInStomach += (ACE.dose_amount) * 1000
-        lastDose = 0
-        dosesTaken += 1
+    checkAutoDosing()
     
-    if not isFed:
-        if lastDose > 20:
-            aceInIntestine += aceInStomach
-            aceInStomach = 0
-        else:
-            lastDose += time
-        
+    if isFed:
+        emptyTime = 90
     else:
-        if lastDose > 90:
-            aceInIntestine += aceInStomach
-            aceInStomach = 0
+       emptyTime = 20
+        
+    remainingStomach = []
+    
+    for doseTime, amount in stomachDoses:
+        timeInStomach = simTime - doseTime
+        
+        if timeInStomach >= emptyTime:
+            intestineDoses.append([simTime, amount])
         else:
-            lastDose += time
-    print("step in stomach done")
+            remainingStomach.append([doseTime, amount])
+            
+    stomachDoses = remainingStomach
+    
+    simTime += time
     
 def intestineStep(time):
-    print("step in intestine done")
+    global intestineDoses, simTime, Ka
+    
+    absorbedTotal = 0.0
+    updatedDoses = []
+    
+    for doseTime, amount in intestineDoses:
+        t0 = simTime - doseTime
+        t1 = t0 + time
+        
+        f0 = 1 - np.exp(-Ka * t0)
+        f1 = 1 - np.exp(-Ka * t1)
+        
+        absorbedFraction = f1-f0
+        absorbedAmount = amount * absorbedFraction
+        absorbedTotal += absorbedAmount
+        
+        amountRemaining = amount * (1-f1)
+        
+        if amountRemaining > 1e-6:
+            updatedDoses.append([doseTime, amountRemaining])
+            
+    intestineDoses = updatedDoses
+    
+    return absorbedTotal
+    
+    
